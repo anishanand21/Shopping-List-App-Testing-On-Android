70444c26a909ffaf5840c894578295b03c1d46b7
Anish <anishanand3733@gmail.com>
2020-09-30 00:31:47 +0530

    Create fake repository for testing

---
+++ app/src/main/java/com/android/shoppinglisttesting/repositories/DefaultShoppingRepository.kt
@@ -0,0 +1,46 @@
+package com.android.shoppinglisttesting.repositories
+
+import androidx.lifecycle.LiveData
+import com.android.shoppinglisttesting.data.local.ShoppingDao
+import com.android.shoppinglisttesting.data.local.ShoppingItem
+import com.android.shoppinglisttesting.data.remote.PixabayAPI
+import com.android.shoppinglisttesting.data.remote.responses.ImageResponse
+import com.android.shoppinglisttesting.other.Resource
+import retrofit2.Response
+import javax.inject.Inject
+
+class DefaultShoppingRepository @Inject constructor(
+    private val shoppingDao: ShoppingDao,
+    private val pixabayAPI: PixabayAPI
+) : ShoppingRepository {
+    override suspend fun insertShoppingItem(shoppingItem: ShoppingItem) {
+        shoppingDao.insertShoppingItem(shoppingItem)
+    }
+
+    override suspend fun deleteShoppingItem(shoppingItem: ShoppingItem) {
+        shoppingDao.deleteShoppingItem(shoppingItem)
+    }
+
+    override fun observeAllShoppingItems(): LiveData<List<ShoppingItem>> {
+        return shoppingDao.observeAllShoppingItems()
+    }
+
+    override fun observeTotalPrice(): LiveData<Float> {
+        return shoppingDao.observeTotalPrice()
+    }
+
+    override suspend fun searchForImage(imageQuery: String): Resource<ImageResponse> {
+        return try {
+            val response = pixabayAPI.searchForImage(imageQuery)
+            if (response.isSuccessful) {
+                response.body()?.let {
+                    return@let Resource.success(it)
+                } ?: Resource.error("An unknown error occured", null)
+            } else {
+                Resource.error("An unknown error occured", null)
+            }
+        } catch (e: Exception) {
+            Resource.error("Couldn't reach the server. Check your internet connection", null)
+        }
+    }
+}
