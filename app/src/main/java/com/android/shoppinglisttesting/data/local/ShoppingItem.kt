0935452ae726bc3ff7f86b572d7b6e3f1a0c38af
Anish <anishanand3733@gmail.com>
2020-09-29 17:09:36 +0530

    Testing Room DB

--- app/src/main/java/com/android/shoppinglisttesting/data/local/ShoppingItem.kt
+++ app/src/main/java/com/android/shoppinglisttesting/data/local/ShoppingItem.kt
@@ -7,7 +7,7 @@ import androidx.room.PrimaryKey
 data class ShoppingItem(
     var name: String,
     var amount: Int,
-    var price: String,
+    var price: Float,
     var imageUrl: String,
     @PrimaryKey(autoGenerate = true)
     val id: Int? = null
