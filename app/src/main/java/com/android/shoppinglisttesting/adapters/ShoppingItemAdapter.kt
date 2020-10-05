819df5c0406c7376a8535b5e2c39fafbff5e777f
Anish <anishanand3733@gmail.com>
2020-10-06 00:05:35 +0530

    Add searching images

--- app/src/main/java/com/android/shoppinglisttesting/adapters/ShoppingItemAdapter.kt
+++ app/src/main/java/com/android/shoppinglisttesting/adapters/ShoppingItemAdapter.kt
@@ -56,7 +56,7 @@ class ShoppingItemAdapter @Inject constructor(
             tvName.text = shoppingItem.name
             val amountText = "${shoppingItem.amount}x"
             tvShoppingItemAmount.text = amountText
-            val priceText = "${shoppingItem.price}€"
+            val priceText = "Rs ${shoppingItem.price}"
             tvShoppingItemPrice.text = priceText
         }
     }
